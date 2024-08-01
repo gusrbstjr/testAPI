4c95439f59352fc287f7593baa28920b7b076dd3
leejeonghun99 <lajaha99@gmail.com>
2024-08-01 17:36:51 +0900

    carttest

--- src/main/java/com/finalproject/projectapitest/kakaopay/jeonghunDTO.java
+++ src/main/java/com/finalproject/projectapitest/kakaopay/jeonghunDTO.java
@@ -3,5 +3,6 @@ package com.finalproject.projectapitest.kakaopay;
 public class jeonghunDTO {
     public static void main(String[] args) {
         System.out.println("이끄어 완전 아약스 아약스한 상황이네여~~~");
+        System.out.println("이끄어 카트 변경 잌잌잌잌");
     }
 }
